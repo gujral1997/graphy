3a016e971c97c39636244ce6195c615d85b98c45
a <anshgujral1997@gmail.com>
2018-06-14 11:18:04 -0400

    fixed key warning

--- lyrical-graphql/client/components/SongList.js
+++ lyrical-graphql/client/components/SongList.js
@@ -6,7 +6,7 @@ class SongList extends Component {
     renderSongs() {
         return this.props.data.songs.map(song=> {
             return(
-                <li>
+                <li key={song.id} className="collection-item">
                     {song.title}
                 </li>
             )
@@ -18,9 +18,9 @@ class SongList extends Component {
         return <div>Loading...</div>
         }
         return (
-            <div>
+            <ul className="collection">
                 {this.renderSongs()}
-            </div>
+            </ul>
         )
     }
 }
@@ -28,6 +28,7 @@ class SongList extends Component {
 const query = gql `
     {
         songs {
+            id
             title
         }
     }
