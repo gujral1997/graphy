237437c5f5ed337cd8a6a3682468835545354835
a <anshgujral1997@gmail.com>
2018-06-13 21:59:47 -0400

    added mutation to add user

--- users/schema/schema.js
+++ users/schema/schema.js
@@ -6,7 +6,8 @@ const axios = require('axios')
      GraphQLString,
      GraphQLInt,
      GraphQLSchema,
-     GraphQLList
+     GraphQLList,
+     GraphQLNonNull
  } = graphql
 
 const CompanyType = new GraphQLObjectType({
@@ -69,6 +70,25 @@ const CompanyType = new GraphQLObjectType({
      }
  })
 
+const mutation = new GraphQLObjectType({
+    name: 'Mutation',
+    fields: {
+        addUser: {
+            type: UserType,
+            args: {
+                firstName: {type: new GraphQLNonNull(GraphQLString)},
+                age: {type: new GraphQLNonNull(GraphQLInt)},
+                companyId: {type: GraphQLString}
+            },
+            resolve(parentValue, {firstName, age}) {
+                return axios.post('http://localhost:3000/users', {firstName, age})
+                    .then(res => res.data)
+            }
+        }
+    }
+})  
+
  module.exports = new GraphQLSchema({
-    query: RootQuery 
+    query: RootQuery,
+    mutation
 })
