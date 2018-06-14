319f888ea64af663bda0b304c6025d77b80fcefd
a <anshgujral1997@gmail.com>
2018-06-14 16:13:16 -0400

    setup basic react router

--- lyrical-graphql/client/index.js
+++ lyrical-graphql/client/index.js
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import ApolloClient from 'apollo-client'
 import {ApolloProvider} from 'react-apollo'
+import {Router, Route, hashHistory, IndexRoute} from 'react-router'
+import App from './components/App'
 
 import SongList from './components/SongList'
 
@@ -10,7 +12,11 @@ const client = new ApolloClient({})
 const Root = () => {
   return (
     <ApolloProvider client={client}>
-      <SongList />
+      <Router history={hashHistory}>
+        <Route path="/" component={App}>
+          <IndexRoute component={SongList} />
+        </Route>
+      </Router>
     </ApolloProvider>
   )
 };
