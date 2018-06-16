966806f34e01d45654a8600c0ad8a337921bd8f0
a <anshgujral1997@gmail.com>
2018-06-16 14:07:53 -0400

    added muatation to add songs at client side

--- lyrical-graphql/client/components/SongCreate.js
+++ lyrical-graphql/client/components/SongCreate.js
@@ -1,15 +1,22 @@
 import React, {Component} from 'react'
+import gql from 'graphql-tag'
 
 class SongCreate extends Component {
     constructor(props) {
         super(props)
         this.state = {title: ''}
     }
+
+    onSubmit(event) {
+        event.preventDefault();
+        
+    }
+
     render() {
         return (
             <div>
                 <h5>Create a New Song </h5>
-                <form>
+                <form onSubmit={this.onSubmit.bind(this)}>
                     <label>Song Title:</label>
                     <input
                     onChange={event=> this.setState({title: event.target.value})}
@@ -20,5 +27,14 @@ class SongCreate extends Component {
         )
     }
 }
+
+const mutation = gql`
+    mutation AddSong($title: String){
+        addSong(title: $title) {
+            id
+            title
+        }
+    }
+`
 
 export default SongCreate
