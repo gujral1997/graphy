6e3de14e3c2d81f5cd1aea41002443581944e66b
a <anshgujral1997@gmail.com>
2018-06-16 17:25:22 -0400

    able to fetch props from react router to Song detail using graphql as a middleware

--- lyrical-graphql/client/components/SongDetail.js
+++ lyrical-graphql/client/components/SongDetail.js
@@ -1,7 +1,10 @@
 import React, {Component} from 'react'
+import {graphql} from 'react-apollo'
+import fetchSong from '../queries/fetchSong'
 
-export default class SongDetail extends Component {
+class SongDetail extends Component {
     render() {
+        console.log(this.props)
         return (
             <div>
                <h3>Song Detail </h3>
@@ -9,3 +12,7 @@ export default class SongDetail extends Component {
         )
     }
 }
+
+export default  graphql(fetchSong, {
+    options: (props) => {return{variables: {id: props.params.id}}}
+})(SongDetail)
