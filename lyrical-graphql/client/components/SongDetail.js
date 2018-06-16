3f5cef7e72e65fd33ac7d225f6b8e1c50da722a5
a <anshgujral1997@gmail.com>
2018-06-16 16:54:42 -0400

    created query to fetch title of song

---
+++ lyrical-graphql/client/components/SongDetail.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+
+export default class SongDetail extends Component {
+    render() {
+        return (
+            <div>
+               <h3>Song Detail </h3>
+            </div>
+        )
+    }
+}
--- lyrical-graphql/client/index.js
+++ lyrical-graphql/client/index.js
@@ -8,6 +8,7 @@ import {Router, Route, hashHistory, IndexRoute} from 'react-router'
 import App from './components/App'
 import SongList from './components/SongList'
 import SongCreate from './components/SongCreate'
+import SongDetail from './components/SongDetail'
 
 const client = new ApolloClient({})
 
@@ -18,6 +19,7 @@ const Root = () => {
         <Route path="/" component={App}>
           <IndexRoute component={SongList} />
           <Route path="song/new" component={SongCreate} />
+          <Route path="song/:id" component={SongDetail} />
         </Route>
       </Router>
     </ApolloProvider>
