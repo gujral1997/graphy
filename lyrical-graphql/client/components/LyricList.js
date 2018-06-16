c43879dbda4d707867321b86b03f2e6284997659
a <anshgujral1997@gmail.com>
2018-06-16 19:53:27 -0400

    added like functionality

--- lyrical-graphql/client/components/LyricList.js
+++ lyrical-graphql/client/components/LyricList.js
@@ -1,20 +1,28 @@
 import React, {Component} from 'react'
+import {graphql} from 'react-apollo'
+import gql from 'graphql-tag'
 
 class LysricList extends Component {
     onLike(id) {
-        console.log(id)
+        this.props.mutate({
+            variables: {id}
+        })
     }
 
     renderLyrics() {
-        return this.props.lyrics.map(({id, content}) => {
+        return this.props.lyrics.map(({id, content, likes}) => {
             return (
                 <li key={id} className="collection-item">
                     {content}
-                    <i
-                    className="material-icons"
-                    onClick={() => this.onLike(id)}
-                    >
-                    thumb_up</i>
+                    <div className="vote-box">
+                        <i
+                        className="material-icons"
+                        onClick={() => this.onLike(id)}
+                        >
+                            thumb_up
+                        </i>
+                        {likes}
+                    </div>
                 </li>
             )
         })
@@ -29,4 +37,13 @@ class LysricList extends Component {
     }
 }
 
-export default LysricList
+const mutation = gql `
+mutation Like ($id:ID) {
+    likeLyric(id:$id) {
+      id
+      likes
+    }
+  }
+`
+
+export default graphql(mutation)(LysricList)
