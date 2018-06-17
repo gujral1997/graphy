aeac2ee2d02ad5ffdfdc8858b6d14feae4609398
a <anshgujral1997@gmail.com>
2018-06-17 17:42:52 -0400

    added signup functionality

--- auth-graphql-starter/server/server.js
+++ auth-graphql-starter/server/server.js
@@ -12,7 +12,7 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = 'mongodb://ansh:ansh1234@ds161610.mlab.com:61610/auth-gql';
 
 // Mongoose's built in promise library is deprecated, replace it with ES2015 Promise
 mongoose.Promise = global.Promise;
