2bab76d6539373978015cb819853527847e26fb9
a <anshgujral1997@gmail.com>
2018-06-21 10:09:56 -0400

    created base template for header

--- auth-graphql-starter/client/components/Header.js
+++ auth-graphql-starter/client/components/Header.js
@@ -3,12 +3,27 @@ import {graphql} from 'react-apollo'
 import query from '../queries/CurrentUser'
 
 class Header extends Component {
+    renderButtons() {
+        const {loading, user} = this.props.data
+        if(loading) {return <div />}
+
+        if(user) {
+            return <div>Logout</div>
+        }
+        else {
+            return (
+                <div>You're not signed in</div>
+            )
+        }
+
+    }
     render () {
-        console.log(this.props.data)
         return (
-            <div>
-                Header
-            </div>
+            <nav>
+                <div className="nav-wrapper">
+                    {this.renderButtons()}
+                </div>
+            </nav>
         )
     }
 } 
