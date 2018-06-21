43a67d2f00a6d7626e970efcd086c948036895b9
a <anshgujral1997@gmail.com>
2018-06-21 09:41:26 -0400

    including cookies with graphql request

---
+++ auth-graphql-starter/client/components/Header.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react'
+import {graphql} from 'react-apollo'
+import query from '../queries/CurrentUser'
+
+class Header extends Component {
+    render () {
+        console.log(this.props.data)
+        return (
+            <div>
+                Header
+            </div>
+        )
+    }
+} 
+
+export default graphql(query)(Header)
